7a19779ab2d8ca348be5acd2dfdc42ea3ad08fe8
rahul7011 <champrahul01@gmail.com>
2023-03-09 14:26:45 +0530

    Initialisation

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const root=ReactDom.createRoot(document.getElementById("root"));
+const heading=<h1>Fun begins from here!</h1>;
+root.render(heading);
