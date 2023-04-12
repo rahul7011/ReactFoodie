a63f10e0bc5502e32fc341b6662cdf6a23d733be
rahul7011 <champrahul01@gmail.com>
2023-04-12 18:36:08 +0530

    refactoring

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,13 +25,13 @@ const Header = () => {
   return (
     <div className="flex justify-between border ">
       <Title />
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <button
             onClick={handleDropDown}
             data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex items-center p-2 ml-3 ml-auto text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 ml-3 ml-auto text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-200 dark:focus:ring-gray-100"
             aria-controls="navbar-default"
             aria-expanded="false"
           >
