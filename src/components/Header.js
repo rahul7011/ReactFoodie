88895a49e11673884458cdbe688f741d58a05b28
rahul7011 <champrahul01@gmail.com>
2023-04-12 18:22:13 +0530

    updating header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import useAuth from "../utils/useAuth";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import UserContext from "../utils/UserContext";
 import { useSelector } from "react-redux";
 import Logo from "../../media/logo.png";
@@ -14,45 +14,90 @@ export const Title = () => (
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useAuth();
   const { user } = useContext(UserContext);
+  const [isOpen, setOpen] = useState(false);
+
+  const handleDropDown = () => {
+    setOpen(!isOpen);
+  };
+
   //Subscribing to the CartSlice
   const cartItems = useSelector((store) => store.cart.items);
   return (
     <div className="flex justify-between border ">
       <Title />
-      <div className="nav-items font-bold">
-        <ul className="flex py-6">
-          <Link to="/">
-            <li className="px-8 hover:text-orange-500">Home</li>
-          </Link>
-          <Link to="/about">
-            <li className="px-8 hover:text-orange-500">About</li>
-          </Link>
-          <Link to="/contact">
-            <li className="px-8 hover:text-orange-500">Contact</li>
-          </Link>
-          <Link to="/cart">
-            <li data-testid="cart" className="px-8 hover:text-orange-500">
-              Cart{cartItems.length > 0 ? " " + cartItems.length : ""}
-            </li>
-          </Link>
-
-          {isLoggedIn === 1 ? (
-            <button
-              className="px-8 font-bold hover:text-red-600"
-              onClick={() => setIsLoggedIn(0)}
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <button
+            onClick={handleDropDown}
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 ml-3 ml-auto text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              {user.name} LogOut
-            </button>
-          ) : (
-            <button
-              className="px-8 font-bold hover:text-green-600"
-              onClick={() => setIsLoggedIn(1)}
-            >
-              Login
-            </button>
-          )}
-        </ul>
-      </div>
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <div
+            className={`${
+              isOpen ? "block" : "hidden"
+            } w-full md:block md:w-auto" id="navbar-default`}
+          >
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <Link to="/">
+                <li
+                  className="block py-2 pl-3 pr-4 text-white bg-orange-500 rounded md:bg-transparent md:text-orange-500 md:p-0 dark:text-white md:dark:text-orange-500"
+                  aria-current="page"
+                >
+                  Home
+                </li>
+              </Link>
+              <Link to="/about">
+                <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  About
+                </li>
+              </Link>
+              <Link to="/contact">
+                <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Contact
+                </li>
+              </Link>
+              <Link to="/cart">
+                <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  Cart{cartItems.length > 0 ? " " + cartItems.length : ""}
+                </li>
+              </Link>
+              {isLoggedIn === 1 ? (
+                <li
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  onClick={() => setIsLoggedIn(0)}
+                >
+                  {user.name} LogOut
+                </li>
+              ) : (
+                <li
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  onClick={() => setIsLoggedIn(1)}
+                >
+                  Login
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
